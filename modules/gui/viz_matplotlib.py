9b27cfe65f7ba4577cc5c161deb3229697bc38e3
cjh1995-ros <dkwnsgur12@gmail.com>
2023-12-07 22:44:32 +0900

    update visualizer

--- modules/gui/viz_matplotlib.py
+++ modules/gui/viz_matplotlib.py
@@ -1,8 +1,9 @@
 from modules.gui import BaseVisualizer
 from typing import Any, List
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-
+from scipy.spatial.transform import Rotation as R
 
 
 
@@ -27,7 +28,70 @@ class MatplotVisualizer(BaseVisualizer):
         plt.show()
     
     def vis_3d(self, images: List[Any], cameras: List[Any], data: Any) -> None:
-        pass
+        """Visualize camera positions, orientations (as rotation matrices), and 3D points."""
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Plot 3D points
+        ax.scatter(data[:, 0], data[:, 1], data[:, 2], color='b')
+
+        # Plot cameras
+        self._draw_cameras(ax, cameras)
+    
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_zlabel('Z')
+        plt.show()
+        
     
     def vis_satellite(self, satellite_image: Any, cameras: List[Any], data: Any) -> None:
-        pass
+        fig, ax = plt.subplots()
+
+        ax.scatter(data[:, 0], data[:, 1], s=1)
+        
+        self._draw_cameras(ax, cameras, length=0.1, dimension=2)
+        
+        # ax.imshow(satellite_image)
+        
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        
+        plt.show()
+
+    
+    def _draw_cameras(self, ax, cameras: List[Any], length: float = 1.0 ,dimension: int = 3):
+        """
+        Plot camera position in ax. cameras is a list of Camera.
+        dimension is either 2 or 3.
+        """
+        for camera in cameras:
+            self._draw_camera(ax, camera, length, dimension)
+
+    def _draw_camera(self, ax, camera: Any, length, dimension):
+        """
+        Plot camera position in ax. camera is a Camera.
+        dimension is either 2 or 3.
+        """
+        assert dimension == 2 or dimension == 3
+
+        Rmat = R.from_rotvec(camera.R).as_matrix()
+        t = camera.t
+        
+        # transform to world coordinate
+        Rmat = Rmat.T # inverse rotation
+        t = -Rmat @ t # inverse translation
+        
+        x, y, z = t
+        
+        if dimension == 2:
+            # Z direction is the direction of the camera in world coord
+            direction = Rmat[:, 2]
+            u, v, w = direction * length
+            ax.quiver(x, y, u, v, color='b')
+        
+        if dimension == 3:
+            for i in range(3):
+                # Extract the direction for each axis
+                direction = Rmat[:, i]
+                u, v, w = direction * length
+                ax.quiver(x, y, z, u, v, w, color=['b', 'g', 'r'][i])
--- synthetic_run.py
+++ synthetic_run.py
@@ -5,44 +5,44 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 
-def plot_camera(ax, position, rotation_matrix, length=1.0):
-    """Plot a representation of a camera with 3 orientation axes."""
-    x, y, z = position
+# def plot_camera(ax, position, rotation_matrix, length=1.0):
+#     """Plot a representation of a camera with 3 orientation axes."""
+#     x, y, z = position
     
-    # rotation_matrix = R.from_rotvec(rvec).as_matrix()
+#     # rotation_matrix = R.from_rotvec(rvec).as_matrix()
     
-    for i in range(3):
-        # Extract the direction for each axis
-        direction = rotation_matrix[:, i]
-        u, v, w = direction * length
-        ax.quiver(x, y, z, u, v, w, color=['b', 'g', 'r'][i])
+#     for i in range(3):
+#         # Extract the direction for each axis
+#         direction = rotation_matrix[:, i]
+#         u, v, w = direction * length
+#         ax.quiver(x, y, z, u, v, w, color=['b', 'g', 'r'][i])
 
-def visualize_cameras_and_points(camera_positions, camera_orientations, points_3d):
-    """Visualize camera positions, orientations (as rotation matrices), and 3D points."""
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+# def visualize_cameras_and_points(camera_positions, camera_orientations, points_3d):
+#     """Visualize camera positions, orientations (as rotation matrices), and 3D points."""
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111, projection='3d')
 
-    # Plot 3D points
-    ax.scatter(points_3d[:, 0], points_3d[:, 1], points_3d[:, 2], color='b')
+#     # Plot 3D points
+#     ax.scatter(points_3d[:, 0], points_3d[:, 1], points_3d[:, 2], color='b')
 
-    # Plot cameras
-    for position, orientation in zip(camera_positions, camera_orientations):
-        plot_camera(ax, position, orientation)
+#     # Plot cameras
+#     for position, orientation in zip(camera_positions, camera_orientations):
+#         plot_camera(ax, position, orientation)
 
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
-    plt.show()
+#     ax.set_xlabel('X')
+#     ax.set_ylabel('Y')
+#     ax.set_zlabel('Z')
+#     plt.show()
 
-# Example data
-camera_positions = np.array([[3, 0, 3], [0, 5, 5]])  # Replace with your camera positions
-# Replace with your camera rotation matrices (3x3)
-camera_orientations = [np.array([[1, 0, 0],
-                                [0, 0, 1],
-                                [0, -1, 0]]),
-                       np.array([[0, 0, 1], 
-                                [1, 0, 0], 
-                                [0, -1, 0]])]
-points_3d = np.random.rand(10, 3) * 10  # Replace with your 3D points
+# # Example data
+# camera_positions = np.array([[3, 0, 3], [0, 5, 5]])  # Replace with your camera positions
+# # Replace with your camera rotation matrices (3x3)
+# camera_orientations = [np.array([[1, 0, 0],
+#                                 [0, 0, 1],
+#                                 [0, -1, 0]]),
+#                        np.array([[0, 0, 1], 
+#                                 [1, 0, 0], 
+#                                 [0, -1, 0]])]
+# points_3d = np.random.rand(10, 3) * 10  # Replace with your 3D points
 
-visualize_cameras_and_points(camera_positions, camera_orientations, points_3d)
+# visualize_cameras_and_points(camera_positions, camera_orientations, points_3d)
